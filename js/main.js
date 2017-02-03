af8d56b4773c8568f501d3d9352e80ea679806c6
Olga Erushenko <olga.erushenko@gmail.com>
2017-02-03 18:12:55 +0200

    Update some information about Schedule app

--- js/main.js
+++ js/main.js
@@ -48,9 +48,10 @@ const obj = {
 		(in this case are hours) and return json file in the same form as the input data(based on of the interval what 
 		selected by user). The selected cell is a darker color, than non-selected cell. Should be a possible to select 
 		the desired range holding the left button of the mouse. Selected range should become dark color. 
-		When we toggle click on the column "all day" all cells of the interval should become selected or non-selected. `,
+		When we toggle click on the column "all day" all cells of the interval should become selected or non-selected. 
+		<mark>Output object you can see in console</mark>`,
 		img: 'img/Schedule.png',
-		list: ['React JS', 'Vanila JS', 'ES6']
+		list: ['React JS', 'Redux', 'WebPack', 'Vanila JS', 'ES6']
 	},
 
 
