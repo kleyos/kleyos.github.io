3cc44d6ef8786cbf148c6dc79db6899fbdb69927
Olga Erushenko <olga.erushenko@gmail.com>
2017-07-28 16:56:18 +0300

    add new obj map

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var obj = {
+	worldmap: {
+		name: 'React-Redux-World-Map-Component',
+		link: 'items/map/',
+		secondName: 'The chart component that showing analysis of the data',
+		description: 'Project based on React/Redux. <br>\n\t\t\t\t\t Requirements: Show map of the world and the cities with population more than 500 000 by default.<br>\n\t\t\t\t\t Also, the component must have a field for filtering. User inputted value of population and we can see markers<br>\n\t\t\t\t\t of the cities where population more or equal this value',
+		img: 'img/map.png',
+		list: ['React JS', 'Redux', 'ES6', 'd3-geo API']
+	},
 	chart: {
 		name: 'React-Redux-Chart-Component',
 		link: 'items/charts/',
 		secondName: 'The chart component that showing analysis of the data',
-		description: 'Project based on React/Redux. <br>\n\t\t\t\t\t Requirements: The slider will give you the number of days \n\t\t\t\t\t for which we need to show the analysis for. When we click on the buttons,<br>\n\t\t\t\t\t it will enable/disable the respective lines. For the sake of this task, you \n\t\t\t\t\t can assume static data.',
+		description: 'Project based on React/Redux. <br>\n\t\t\t\t\t Requirements: The slider will give you the number of days\n\t\t\t\t\t for which we need to show the analysis for. When we click on the buttons,<br>\n\t\t\t\t\t it will enable/disable the respective lines. For the sake of this task, you\n\t\t\t\t\t can assume static data.',
 		img: 'img/chart.png',
 		list: ['React JS', 'Redux', 'WebPack', 'Vanilla JS', 'ES6', 'Charts']
 	},
@@ -13,7 +21,7 @@ var obj = {
 		name: 'React-Redux-Table-Component',
 		link: 'items/table/',
 		secondName: 'The table component with filtering, sorting, paging and acync functions',
-		description: 'Project via React/Redux/Routing. <br>\n\t\t\t\t\t Requirements: To write a table component which fetching data and   \n\t\t\t\t\t building the table. Table should have additional abilities for users like sorting, \n\t\t\t\t\t filtering and pagination. <br>\n\t\t\t\t\t When the user enters on the page he selects the dataset: small, large or own.\n\t\t\t\t\t Then data is loaded from the server and according to the data is constructed the table.</br>\n\t\t\t\t\t The user can click on the table line and see information in a block under the table',
+		description: 'Project via React/Redux/Routing. <br>\n\t\t\t\t\t Requirements: To write a table component which fetching data and\n\t\t\t\t\t building the table. Table should have additional abilities for users like sorting,\n\t\t\t\t\t filtering and pagination. <br>\n\t\t\t\t\t When the user enters on the page he selects the dataset: small, large or own.\n\t\t\t\t\t Then data is loaded from the server and according to the data is constructed the table.</br>\n\t\t\t\t\t The user can click on the table line and see information in a block under the table',
 		img: 'img/table.png',
 		list: ['React JS', 'Redux', 'Fetch API', 'WebPack', 'Vanilla JS', 'ES6']
 	},
@@ -22,7 +30,7 @@ var obj = {
 		name: 'React App - Schedule Time Sheet',
 		link: 'items/Schedule/index.html',
 		secondName: 'Time Sheet. A component that will draw a weekly cycle',
-		description: 'Project via ReactJS.\n\t\t\t\t\t  Requirements: To write a component that will draw a weekly \n\t\t\t\t\t  cycle (calendar for the week). \n\t\t\t\t\tInput data JSON \n\t\t<br><code>{<br>\n\t\t "mo": [\n\t\t      {\n\t\t        "bt": 240,\n\t\t        "et": 779\n\t\t      }\n\t\t ],<br>\n\t\t "tu": [\n\t\t ],<br>\n\t\t "we": [\n\t\t ],<br>\n\t\t "th": [\n\t\t   {\n\t\t  "bt": 240,\n\t\t        "et": 779\n\t\t   },\n\t\t   {\n\t\t  "bt": 1140,\n\t\t        "et": 1319\n\t\t   }\n\t\t ],<br>\n\t\t "fr": [\n\t\t   {\n\t\t  "bt": 660,\n\t\t        "et": 1019\n\t\t   }\n\t\t ],<br>\n\t\t "sa": [\n\t\t      {\n\t\t        "bt": 0,\n\t\t        "et": 1439\n\t\t      }\n\t\t    ],<br>\n\t\t "su": []<br>\n\t\t}</code> <br> \n\t\tThe interval of the one cell is one hour. In JSON file pass 59 minutes. Sample:\n\t\tInterval of hours 1:00 to 2:00 = 60 to 119 \n\t\tMinute of the all day (0 - 1439). \n\t\tAlso, the component must be clickable, that is to give the user the opportunity to select the desired interval \n\t\t(in this case are hours) and return json file in the same form as the input data(based on of the interval what \n\t\tselected by user). The selected cell is a darker color, than non-selected cell. Should be a possible to select \n\t\tthe desired range holding the left button of the mouse. Selected range should become dark color. \n\t\tWhen we toggle click on the column "all day" all cells of the interval should become selected or non-selected. \n\t\t<mark>Output object you can see in console</mark>',
+		description: 'Project via ReactJS.\n\t\t\t\t\t  Requirements: To write a component that will draw a weekly\n\t\t\t\t\t  cycle (calendar for the week).\n\t\t\t\t\tInput data JSON\n\t\t<br><code>{<br>\n\t\t "mo": [\n\t\t      {\n\t\t        "bt": 240,\n\t\t        "et": 779\n\t\t      }\n\t\t ],<br>\n\t\t "tu": [\n\t\t ],<br>\n\t\t "we": [\n\t\t ],<br>\n\t\t "th": [\n\t\t   {\n\t\t  "bt": 240,\n\t\t        "et": 779\n\t\t   },\n\t\t   {\n\t\t  "bt": 1140,\n\t\t        "et": 1319\n\t\t   }\n\t\t ],<br>\n\t\t "fr": [\n\t\t   {\n\t\t  "bt": 660,\n\t\t        "et": 1019\n\t\t   }\n\t\t ],<br>\n\t\t "sa": [\n\t\t      {\n\t\t        "bt": 0,\n\t\t        "et": 1439\n\t\t      }\n\t\t    ],<br>\n\t\t "su": []<br>\n\t\t}</code> <br>\n\t\tThe interval of the one cell is one hour. In JSON file pass 59 minutes. Sample:\n\t\tInterval of hours 1:00 to 2:00 = 60 to 119\n\t\tMinute of the all day (0 - 1439).\n\t\tAlso, the component must be clickable, that is to give the user the opportunity to select the desired interval\n\t\t(in this case are hours) and return json file in the same form as the input data(based on of the interval what\n\t\tselected by user). The selected cell is a darker color, than non-selected cell. Should be a possible to select\n\t\tthe desired range holding the left button of the mouse. Selected range should become dark color.\n\t\tWhen we toggle click on the column "all day" all cells of the interval should become selected or non-selected.\n\t\t<mark>Output object you can see in console</mark>',
 		img: 'img/Schedule.png',
 		list: ['React JS', 'Redux', 'WebPack', 'Vanilla JS', 'ES6']
 	},
@@ -30,7 +38,7 @@ var obj = {
 		name: 'List of Star-Wars Personages',
 		link: 'items/plastic-jam/',
 		secondName: 'Using SWAPI',
-		description: 'The SPA via React/Redux. <br>\n\t\t\t\t\t Requirements: To write aa test site with a list of the Star Wars personages \n\t\t\t\t\t from this https://swapi.co.   \n\t\t\t\t\t 1. use any template; \n\t\t\t\t\t 2.add checkbox \u201Clike\u201D to every row in your list. \n\t\t\t\t\t Checkbox should save its state after page reloading;\n\t\t\t\t\t 3. add animation (on your own);\n\t\t\t\t\t 4. pass the validators\u2019 tests without mistakes;\n\t\t\t\t\t 5.deploy to any server;',
+		description: 'The SPA via React/Redux. <br>\n\t\t\t\t\t Requirements: To write aa test site with a list of the Star Wars personages\n\t\t\t\t\t from this https://swapi.co.\n\t\t\t\t\t 1. use any template;\n\t\t\t\t\t 2.add checkbox \u201Clike\u201D to every row in your list.\n\t\t\t\t\t Checkbox should save its state after page reloading;\n\t\t\t\t\t 3. add animation (on your own);\n\t\t\t\t\t 4. pass the validators\u2019 tests without mistakes;\n\t\t\t\t\t 5.deploy to any server;',
 		img: '',
 		list: ['React JS', 'Redux', 'Fetch API', 'WebPack', 'Vanilla JS', 'ES6']
 	},
@@ -38,7 +46,7 @@ var obj = {
 		name: 'The Gallery',
 		link: 'items/Gallery/',
 		secondName: '',
-		description: 'Project via ReactJS. <br>\n\t\t\t\t\t Requirements: To write a component using  fetch() to get data and   \n\t\t\t\t\t show the gallery of pictures. Press any picture and show selected picture using \n\t\t\t\t\t react-routing.<br>\n\t\t\t\t\t This project was built via React-Native too. You can see on my Github page',
+		description: 'Project via ReactJS. <br>\n\t\t\t\t\t Requirements: To write a component using  fetch() to get data and\n\t\t\t\t\t show the gallery of pictures. Press any picture and show selected picture using\n\t\t\t\t\t react-routing.<br>\n\t\t\t\t\t This project was built via React-Native too. You can see on my Github page',
 		img: 'img/gallery.gif',
 		list: ['React JS', 'Redux', 'Fetch API', 'React-Routing', 'WebPack', 'Vanilla JS', 'ES6']
 	},
@@ -65,7 +73,7 @@ var obj = {
 		name: 'Колл центр Omega Contact',
 		link: 'http://kley.ho.ua/Omega/',
 		secondName: 'Landing Page Omega Contact.',
-		description: 'Development of landing page for company Contact Omega. \n\t\t              A Mobile First approach . Adaptability. \n\t\t              Cross-browser compatibility: latest versions of IE, Safari, Chrome and FF; \n\t\t              mobile Safari (iOS 8) and Chrome (Android 5). \n\t\t              The project used libraries: jasny-bootstrap, font-awesome, equalHeights, mb.ytplayer. \n\t\t              BEM, using SCSS for CSS. Build via Grunt.',
+		description: 'Development of landing page for company Contact Omega.\n\t\t              A Mobile First approach . Adaptability.\n\t\t              Cross-browser compatibility: latest versions of IE, Safari, Chrome and FF;\n\t\t              mobile Safari (iOS 8) and Chrome (Android 5).\n\t\t              The project used libraries: jasny-bootstrap, font-awesome, equalHeights, mb.ytplayer.\n\t\t              BEM, using SCSS for CSS. Build via Grunt.',
 		img: 'img/Omega.png',
 		list: ['HTML5', 'CSS3', 'jQuery', ' Bootstrap 3 Grid', 'Ajax', 'PHP', 'Sass', 'Grunt']
 	},
@@ -91,7 +99,7 @@ var _loop = function _loop(ob) {
 
 	var listItems = obj[ob].list;
 
-	var htmlPanel = '<div class="panel-heading">\n\t\t\t\t\t\t    <h3 class="item__title panel-title">\n\t\t\t\t\t\t        <a class="item__link" href="' + link + '">' + name + '</a>\n\t\t\t\t\t\t    </h3>\n\t\t\t\t\t    </div>\n\t\t\t\t      \t<div class="panel-body">\n\t\t\t\t          <ul class="item__list"></ul>\n\t\t\t\t          <div class="item__description">\n\t\t\t\t            <p>' + secondName + '</p>\n\t\t\t\t            <p>' + description + '</p>\n\t\t\t\t          </div>\n\t\t\t\t          <div class="link">\n\t\t\t\t            <a class="item__link" href="' + link + '">\n\t\t\t\t            \t<img class="link__img" src="' + img + '" alt="site screen">\n\t\t\t\t            </a>\n\t\t\t\t          </div> \n\t\t\t\t     \t</div>';
+	var htmlPanel = '<div class="panel-heading">\n\t\t\t\t\t\t    <h3 class="item__title panel-title">\n\t\t\t\t\t\t        <a class="item__link" href="' + link + '">' + name + '</a>\n\t\t\t\t\t\t    </h3>\n\t\t\t\t\t    </div>\n\t\t\t\t      \t<div class="panel-body">\n\t\t\t\t          <ul class="item__list"></ul>\n\t\t\t\t          <div class="item__description">\n\t\t\t\t            <p>' + secondName + '</p>\n\t\t\t\t            <p>' + description + '</p>\n\t\t\t\t          </div>\n\t\t\t\t          <div class="link">\n\t\t\t\t            <a class="item__link" href="' + link + '">\n\t\t\t\t            \t<img class="link__img" src="' + img + '" alt="site screen">\n\t\t\t\t            </a>\n\t\t\t\t          </div>\n\t\t\t\t     \t</div>';
 
 	var wrapper = document.querySelector('.wrapper');
 	var div = document.createElement('div');
--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 const obj = {
+	worldmap:{
+		name:'React-Redux-World-Map-Component',
+		link:'items/map/',
+		secondName: `The chart component that showing analysis of the data`,
+		description:`Project based on React/Redux. <br>
+					 Requirements: Show map of the world and the cities with population more than 500 000 by default.<br>
+					 Also, the component must have a field for filtering. User inputted value of population and we can see markers<br>
+					 of the cities where population more or equal this value`,
+		img: 'img/map.png',
+		list:['React JS', 'Redux', 'ES6', 'd3-geo API']
+	},
 	chart:{
 		name:'React-Redux-Chart-Component',
 		link:'items/charts/',
 		secondName: `The chart component that showing analysis of the data`,
 		description:`Project based on React/Redux. <br>
-					 Requirements: The slider will give you the number of days 
+					 Requirements: The slider will give you the number of days
 					 for which we need to show the analysis for. When we click on the buttons,<br>
-					 it will enable/disable the respective lines. For the sake of this task, you 
+					 it will enable/disable the respective lines. For the sake of this task, you
 					 can assume static data.`,
 		img: 'img/chart.png',
 		list:['React JS', 'Redux', 'WebPack', 'Vanilla JS', 'ES6', 'Charts']
@@ -16,8 +27,8 @@ const obj = {
 		link:'items/table/',
 		secondName: `The table component with filtering, sorting, paging and acync functions`,
 		description:`Project via React/Redux/Routing. <br>
-					 Requirements: To write a table component which fetching data and   
-					 building the table. Table should have additional abilities for users like sorting, 
+					 Requirements: To write a table component which fetching data and
+					 building the table. Table should have additional abilities for users like sorting,
 					 filtering and pagination. <br>
 					 When the user enters on the page he selects the dataset: small, large or own.
 					 Then data is loaded from the server and according to the data is constructed the table.</br>
@@ -25,15 +36,15 @@ const obj = {
 		img: 'img/table.png',
 		list:['React JS', 'Redux', 'Fetch API', 'WebPack', 'Vanilla JS', 'ES6']
 	},
-	
+
 	Schedule: {
 		name: 'React App - Schedule Time Sheet',
 		link: 'items/Schedule/index.html',
 		secondName: 'Time Sheet. A component that will draw a weekly cycle' ,
 		description: `Project via ReactJS.
-					  Requirements: To write a component that will draw a weekly 
-					  cycle (calendar for the week). 
-					Input data JSON 
+					  Requirements: To write a component that will draw a weekly
+					  cycle (calendar for the week).
+					Input data JSON
 		<br><code>{<br>
 		 "mo": [
 		      {
@@ -68,15 +79,15 @@ const obj = {
 		      }
 		    ],<br>
 		 "su": []<br>
-		}</code> <br> 
+		}</code> <br>
 		The interval of the one cell is one hour. In JSON file pass 59 minutes. Sample:
-		Interval of hours 1:00 to 2:00 = 60 to 119 
-		Minute of the all day (0 - 1439). 
-		Also, the component must be clickable, that is to give the user the opportunity to select the desired interval 
-		(in this case are hours) and return json file in the same form as the input data(based on of the interval what 
-		selected by user). The selected cell is a darker color, than non-selected cell. Should be a possible to select 
-		the desired range holding the left button of the mouse. Selected range should become dark color. 
-		When we toggle click on the column "all day" all cells of the interval should become selected or non-selected. 
+		Interval of hours 1:00 to 2:00 = 60 to 119
+		Minute of the all day (0 - 1439).
+		Also, the component must be clickable, that is to give the user the opportunity to select the desired interval
+		(in this case are hours) and return json file in the same form as the input data(based on of the interval what
+		selected by user). The selected cell is a darker color, than non-selected cell. Should be a possible to select
+		the desired range holding the left button of the mouse. Selected range should become dark color.
+		When we toggle click on the column "all day" all cells of the interval should become selected or non-selected.
 		<mark>Output object you can see in console</mark>`,
 		img: 'img/Schedule.png',
 		list: ['React JS', 'Redux', 'WebPack', 'Vanilla JS', 'ES6']
@@ -86,10 +97,10 @@ const obj = {
 		link:'items/plastic-jam/',
 		secondName: `Using SWAPI`,
 		description:`The SPA via React/Redux. <br>
-					 Requirements: To write aa test site with a list of the Star Wars personages 
-					 from this https://swapi.co.   
-					 1. use any template; 
-					 2.add checkbox “like” to every row in your list. 
+					 Requirements: To write aa test site with a list of the Star Wars personages
+					 from this https://swapi.co.
+					 1. use any template;
+					 2.add checkbox “like” to every row in your list.
 					 Checkbox should save its state after page reloading;
 					 3. add animation (on your own);
 					 4. pass the validators’ tests without mistakes;
@@ -102,14 +113,14 @@ const obj = {
 		link:'items/Gallery/',
 		secondName: ``,
 		description:`Project via ReactJS. <br>
-					 Requirements: To write a component using  fetch() to get data and   
-					 show the gallery of pictures. Press any picture and show selected picture using 
+					 Requirements: To write a component using  fetch() to get data and
+					 show the gallery of pictures. Press any picture and show selected picture using
 					 react-routing.<br>
 					 This project was built via React-Native too. You can see on my Github page`,
 		img: 'img/gallery.gif',
 		list:['React JS', 'Redux', 'Fetch API', 'React-Routing', 'WebPack', 'Vanilla JS', 'ES6']
 	},
-	
+
 	pinPong: {
 		name: 'React App - PinPong Game',
 		link: 'items/new-pinpong-app/index.html',
@@ -132,11 +143,11 @@ const obj = {
 		name: 'Колл центр Omega Contact',
 		link: 'http://kley.ho.ua/Omega/',
 		secondName: 'Landing Page Omega Contact.',
-		description: `Development of landing page for company Contact Omega. 
-		              A Mobile First approach . Adaptability. 
-		              Cross-browser compatibility: latest versions of IE, Safari, Chrome and FF; 
-		              mobile Safari (iOS 8) and Chrome (Android 5). 
-		              The project used libraries: jasny-bootstrap, font-awesome, equalHeights, mb.ytplayer. 
+		description: `Development of landing page for company Contact Omega.
+		              A Mobile First approach . Adaptability.
+		              Cross-browser compatibility: latest versions of IE, Safari, Chrome and FF;
+		              mobile Safari (iOS 8) and Chrome (Android 5).
+		              The project used libraries: jasny-bootstrap, font-awesome, equalHeights, mb.ytplayer.
 		              BEM, using SCSS for CSS. Build via Grunt.`,
     	img: 'img/Omega.png',
     	list: ['HTML5','CSS3','jQuery',' Bootstrap 3 Grid', 'Ajax', 'PHP', 'Sass', 'Grunt']
@@ -173,7 +184,7 @@ for (const ob in obj){
 				            <a class="item__link" href="${link}">
 				            	<img class="link__img" src="${img}" alt="site screen">
 				            </a>
-				          </div> 
+				          </div>
 				     	</div>`;
 
 	const wrapper = document.querySelector('.wrapper');
